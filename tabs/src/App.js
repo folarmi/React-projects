e9c7140642b31bd5a9984b5fcb05657f733dd90b
folarmi <fola4ril@gmail.com>
2020-11-18 12:28:22 +0100

    folder restructure

---
+++ tabs/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Experience from "./Experience";
+
+function App() {
+  return (
+    <div>
+      <Experience />
+    </div>
+  );
+}
+
+export default App;
